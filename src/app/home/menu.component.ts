2f4d71847c12b55e1d9964f1c98075dd599109fc
garycuthbert <garycuthbert@users.noreply.github.com>
2022-06-08 14:43:26 +0100

    Added menu component called from shell.component template

--- src/app/home/menu.component.ts
+++ src/app/home/menu.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  pageTitle = 'Ready to Run!';
+  
   constructor() { }
 
   ngOnInit(): void {
