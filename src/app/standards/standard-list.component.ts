aa3b9b68e834b50ecea85a6f4f2e33852fa80adc
garycuthbert <gary.cuthbert@virgin.net>
2022-06-09 21:37:06 +0100

    Added component to list standards

--- src/app/standards/standard-list.component.ts
+++ src/app/standards/standard-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IExercise } from '../exercises/exercise';
+import { ExerciseService } from '../exercises/exercise.service';
+import { IStandard } from './standard';
+import { StandardService } from './standard.service';
 
 @Component({
   selector: 'app-standard-list',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./standard-list.component.css']
 })
 export class StandardListComponent implements OnInit {
+  pageTitle = 'Standards List';
+  errorMessage: string = '';
+  standards: IStandard[] | undefined;
+  excerises: IExercise[] | undefined;
 
-  constructor() { }
+  constructor(private standardService: StandardService,
+              private exerciseService: ExerciseService) { }
 
   ngOnInit(): void {
+    this.standardService.getStandards().subscribe({
+      next: (standards: IStandard[]) => {
+        this.standards = standards;
+      },
+      error: err => this.errorMessage = err
+    });
+
+    this.exerciseService.getExercises().subscribe({
+      next: (exercises: IExercise[]) => {
+        this.excerises = exercises;
+      },
+      error: err => this.errorMessage = err
+    });
   }
 
 }
