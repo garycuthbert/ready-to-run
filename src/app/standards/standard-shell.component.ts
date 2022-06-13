2beae94dd744ce9b34c5ddac51fc40c659588c78
garycuthbert <gary.cuthbert@virgin.net>
2022-06-13 23:26:19 +0100

    top level view for standards details, wip

---
+++ src/app/standards/standard-shell.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rtr-standard-shell',
+  templateUrl: './standard-shell.component.html',
+  styleUrls: ['./standard-shell.component.css']
+})
+export class StandardShellComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
