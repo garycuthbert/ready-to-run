99454b2ffcede23f8b3b1bf52d08dbc140cd3a2e
garycuthbert <gary.cuthbert@virgin.net>
2022-06-13 23:32:50 +0100

    Added AllStandardsStandard definiiton to optionall include when we want our standard selection to include an 'All' option (used on exercises view)

--- src/app/standards/standard.ts
+++ src/app/standards/standard.ts
@@ -4,3 +4,10 @@ export interface IStandard {
   title: string;
   question?: string;
 }
+
+export const AllStandardsStandard: IStandard = {
+  id: -1,
+  number: 0,
+  title: "All",
+  question: undefined
+}
