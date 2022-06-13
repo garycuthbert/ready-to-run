b9f21ea1519a01cf1a2e5be3b7d013fb21e60aef
garycuthbert <gary.cuthbert@virgin.net>
2022-06-13 23:34:27 +0100

    Updated to include new view structure

--- src/app/standards/standards.module.ts
+++ src/app/standards/standards.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StandardListComponent } from './standard-list.component';
+import { StandardShellComponent } from './standard-shell.component';
+import { StandardDetailShellComponent } from './standard-detail-shell.component';
+import { StandardSummaryListComponent } from './standard-summary-list.component';
 import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    StandardListComponent
+    StandardListComponent,
+    StandardShellComponent,
+    StandardDetailShellComponent,
+    StandardSummaryListComponent
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild([
-      { path: '', component: StandardListComponent }
-    ])
+    RouterModule
+    // RouterModule.forChild([
+    //   { path: 'summary', component: StandardListComponent },
+    //   { path: 'standards', component: StandardShellComponent }
+    //   // { path: '/exercises', component: }
+    // ])
   ]
 })
 export class StandardsModule { }
