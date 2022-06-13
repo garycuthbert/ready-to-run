8f657ea35fe8ed6e76a79808711944bfd14edc3c
garycuthbert <gary.cuthbert@virgin.net>
2022-06-13 23:21:11 +0100

    Added to faked data with exercise, steps and exercise steps defintions

--- src/app/fakedData/faked-data.ts
+++ src/app/fakedData/faked-data.ts
@@ -1,6 +1,7 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { IStandard } from '../standards/standard';
 import { IExercise } from '../exercises/exercise';
+import { IStep } from '../shared/step';
 
 export class FakedData implements InMemoryDbService {
   createDb() {
@@ -84,14 +85,93 @@ export class FakedData implements InMemoryDbService {
     const exercises: IExercise[] = [
       {
         id: 1,
-        title: 'Basic Squat'
+        title: 'Basic Squat',
+        introduction: undefined,
+        steps: []
       },
       {
         id: 2,
-        title: 'Skipping'
+        title: 'Skipping',
+        introduction: undefined,
+        steps: []
+      },
+      {
+        id: 3,
+        title: 'Couch Stretch',
+        introduction: undefined,
+        steps: []
       }
     ];
 
-    return { standards: standards, exercises: exercises };
+    const steps: IStep[] = [
+      {
+        id: 1,
+        imagePath: '/assets/images/couchStretch/step1.png',
+        paragraphs: [
+          "Back your feet up against a wall, a box or the upper cushion " +
+          "of you couch. If you are on a hard floor put down a cushion " +
+          "for your knee."
+        ]
+      },
+      {
+        id: 2,
+        imagePath: '/assets/images/couchStretch/step2.png',
+        paragraphs: [
+          "Slide your left leg so that your knee fits into the corner where " +
+          "the floor meets the wall (or whatever corner you're using). Make " +
+          "your shin flush with the wall and point your toe.",
+
+          "IMPORTANT: Squeeze your glutes, in particular your left glute. " +
+          "Keep squeezing throughout the mobilization. This will stabilize " +
+          "your lower back and position your hip joint correctly"
+        ]
+      },
+      {
+        id: 3,
+        imagePath: '/assets/images/couchStretch/step3.png',
+        paragraphs: [
+          "Draw up your right leg and position it in front of you, with " +
+          "your shin vertical."
+        ]
+      },
+      {
+        id: 4,
+        imagePath: '/assets/images/couchStretch/step4.png',
+        paragraphs: [
+          "With your butt squeezed, drive the front of your hip toward the " +
+          "ground. Maintain this position for at least one minute"
+        ]
+      },
+      {
+        id: 5,
+        imagePath: '/assets/images/couchStretch/step5.png',
+        paragraphs: [
+          "Really crank up you hip flexor by lifting your torso (with " +
+          "your glutes still engaged) and hold for another minute."
+        ]
+      },
+      {
+        id: 6,
+        imagePath: '/assets/images/couchStretch/step6.png',
+        paragraphs: [
+          "Drive your torso upright with your glutes and abdominals " +
+          "engaged."
+        ]
+      }
+    ];
+
+    const exerciseSteps: { exerciseId: number, stepIds: number[] }[] = [
+      {
+        exerciseId: 1,
+        stepIds: [1, 2, 3, 4, 5, 6]
+      }
+    ];
+
+    return {
+      standards: standards,
+      exercises: exercises,
+      steps: steps,
+      exerciseSteps: exerciseSteps
+    };
   }
 }
