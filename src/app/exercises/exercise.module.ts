eb162240d2240cc48c6c4c2b0187aa708012980c
garycuthbert <garycuthbert@users.noreply.github.com>
2022-06-14 14:27:05 +0100

    Updating to include exercise-card/cards

--- src/app/exercises/exercise.module.ts
+++ src/app/exercises/exercise.module.ts
@@ -4,13 +4,17 @@ import { ExerciseListComponent } from './exercise-list.component';
 import { ExerciseShellComponent } from './exercise-shell.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ExerciseCardsComponent } from './exercise-cards.component';
+import { ExerciseCardComponent } from './exercise-card.component';
 
 
 
 @NgModule({
   declarations: [
     ExerciseListComponent,
-    ExerciseShellComponent
+    ExerciseShellComponent,
+    ExerciseCardsComponent,
+    ExerciseCardComponent
   ],
   imports: [
     CommonModule
