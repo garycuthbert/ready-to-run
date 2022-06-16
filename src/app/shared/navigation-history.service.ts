a142f471b4a0aa5e0f3323a8b9a927d246bf4e3b
garycuthbert <gary.cuthbert@virgin.net>
2022-06-16 22:44:42 +0100

    Added service to record in application navigation end points to facilitate back button functionality

---
+++ src/app/shared/navigation-history.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavigationHistoryService {
+  private history: string[] = [];
+
+  constructor(private router: Router, private location: Location) { }
+
+  public startSaveHistory(): void {
+    this.router.events.subscribe(
+      (event) => {
+        if (event instanceof NavigationEnd) {
+          this.history.push(event.urlAfterRedirects);
+        }
+      }
+    );
+  }
+
+  public getHistory(): string[] {
+    return this.history;
+  }
+
+  public goBack(): void {
+    this.history.pop();
+
+    if (this.history.length > 0) {
+      this.location.back();
+    } else {
+      this.router.navigateByUrl("/");
+    }
+  }
+
+  public getPerviousUrl(): string {
+    if (this.history.length > 0) {
+      return this.history[this.history.length - 2];
+    }
+
+    return '';
+  }
+}
