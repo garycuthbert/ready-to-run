ca17c80c0406150875176e98e989d1904b472ce4
garycuthbert <gary.cuthbert@virgin.net>
2022-06-16 22:45:12 +0100

    Starting navigation history recording in AppComponent constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { NavigationHistoryService } from './shared/navigation-history.service';
 
 @Component({
   selector: 'rtr-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private navigationHistory : NavigationHistoryService) {
+    this.navigationHistory.startSaveHistory();
+  }
+}
