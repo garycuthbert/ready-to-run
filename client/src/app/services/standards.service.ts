fcc5e4746ffea14bda7841b1c42ba991edd4f9b6
garycuthbert <gary.cuthbert@virgin.net>
2022-09-13 23:18:01 +0100

    Added support for 'getStandard'

--- client/src/app/services/standards.service.ts
+++ client/src/app/services/standards.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, EMPTY, Observable, of } from "rxjs";
+import { BehaviorSubject, catchError, EMPTY, Observable, of, tap } from "rxjs";
 import { ReadyToRunDTOs } from "@shared/model/ReadyToRunDTOs";
 
 @Injectable({
@@ -46,11 +46,28 @@ export class StandardsService {
     }
 
     const url = `${this.standardsUrl}/${id}`;
-    /*return this.http.get<ReadyToRunDTOs.IStandard>(url)
+    return this.http.get<ReadyToRunDTOs.IStandard>(url)
       .pipe(
         tap(data => console.log('getStandard: ' + JSON.stringify(data))),
         catchError(this.handleError)
-      );*/
+      );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage: string;
+
+    if (err.error instanceof Error) {
+        // A client-side or network error occurred. Handle it accordingly.
+        errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong,
+        errorMessage = `Backend returned code ${err.status}, body was: ${err.error}`;
+    }
+    console.error(errorMessage);
+
     return EMPTY;
   }
 
