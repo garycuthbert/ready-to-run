4174b64d5cae6402d999731655fb77606eaba92f
garycuthbert <gary.cuthbert@virgin.net>
2022-09-14 22:51:13 +0100

    WIP component making api call to get steps for relevant exercise, still displaying hardoded steps

--- client/src/app/exercises/exercise-detail.component.ts
+++ client/src/app/exercises/exercise-detail.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavigationHistoryService } from '../shared/navigation-history.service';
 import { ReadyToRunDTOs } from '@shared/model/ReadyToRunDTOs';
+import { ExerciseStepsService } from 'app/services/exercise-steps.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'rtr-exercise-detail',
   templateUrl: './exercise-detail.component.html',
   styleUrls: ['./exercise-detail.component.css']
 })
-export class ExerciseDetailComponent implements OnInit {
+export class ExerciseDetailComponent implements OnInit, OnDestroy {
+
+  private subscription$ : Subscription = new Subscription();
+  exerciseSteps: ReadyToRunDTOs.IExerciseSteps | null = null;
 
   exercise: ReadyToRunDTOs.IExercise = {
     id: 1,
@@ -73,8 +78,11 @@ export class ExerciseDetailComponent implements OnInit {
     ]
   }
 
-  constructor(private route: ActivatedRoute,
-              private navHistory: NavigationHistoryService) {
+  constructor(
+    private route: ActivatedRoute,
+    private navHistory: NavigationHistoryService,
+    private exerciseStepsService: ExerciseStepsService
+    ) {
     console.log('ExerciseDetailComponent constructed!');
   }
 
@@ -82,6 +90,19 @@ export class ExerciseDetailComponent implements OnInit {
     const param = this.route.snapshot.paramMap.get('id');
     let id = param ? +param : 0;
     console.log('exercise details for id = ' + id);
+
+    this.subscription$.add(
+      this.exerciseStepsService.getExerciseSteps(id).subscribe(
+        (steps) => {
+          this.exerciseSteps = steps;
+          console.log('exercisSteps = ', steps);
+        }
+      )
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription$.unsubscribe();
   }
 
   accordionId(): string {
