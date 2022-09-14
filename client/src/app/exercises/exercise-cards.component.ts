0c5aaabf7940f0fa739c6f9306f9c259d52cfe0c
garycuthbert <gary.cuthbert@virgin.net>
2022-09-14 22:51:42 +0100

    Now calling api to get list of exercises to display

--- client/src/app/exercises/exercise-cards.component.ts
+++ client/src/app/exercises/exercise-cards.component.ts
@@ -1,38 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationHistoryService } from '../shared/navigation-history.service';
 import { ReadyToRunDTOs } from '@shared/model/ReadyToRunDTOs';
+import { ExercisesService } from 'app/services/exercises.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'rtr-exercise-cards',
   templateUrl: './exercise-cards.component.html',
   styleUrls: ['./exercise-cards.component.css']
 })
-export class ExerciseCardsComponent implements OnInit {
-  exercises: ReadyToRunDTOs.IExercise[] = [
-    {
-      id: 1,
-      title: 'Basic Squat',
-      introduction: undefined,
-      steps: []
-    },
-    {
-      id: 2,
-      title: 'Skipping',
-      introduction: undefined,
-      steps: []
-    },
-    {
-      id: 3,
-      title: 'Couch Stretch',
-      introduction: "The couch stretch is a weapons-grade technique to "+
-        "open up the hip and open up some slack upstream of the knee.",
-      steps: []
-    }
-  ];
+export class ExerciseCardsComponent implements OnInit, OnDestroy {
 
-  constructor(private navHistory: NavigationHistoryService) { }
+  //exerciseList = new Array<ReadyToRunDTOs.IExercise>();
+  exerciseList : ReadyToRunDTOs.IExercise[] = [];
+
+  private subscriptions$: Subscription = new Subscription();
+
+  constructor(
+    private navHistory: NavigationHistoryService,
+    private excerciseService: ExercisesService
+    ) {
+  }
 
   ngOnInit(): void {
+    this.subscriptions$.add(
+      this.excerciseService.exercises.subscribe(
+        exercises => {
+          //console.log('ngOnInit subscribe! got exercises', this.exerciseList);
+          if (this.exerciseList?.length > 0) { // for some reason without the ?. cehck lenght is reported as null here, look like something in the observable stack as the array is good
+            this.exerciseList.splice(0, this.exerciseList.length);
+          }
+
+          this.exerciseList = JSON.parse(JSON.stringify(exercises));
+        }
+      ));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions$.unsubscribe();
   }
 
   onBack(): void {
