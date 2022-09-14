ab5a9259f52f234e97cf7322c74f587aadd3be1d
garycuthbert <gary.cuthbert@virgin.net>
2022-09-14 22:49:15 +0100

    Added clietn side service to manage calls to exercise steps lookup data

---
+++ client/src/app/services/exercise-steps.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError, EMPTY, Observable, tap } from "rxjs";
+
+import { ReadyToRunDTOs } from "@shared/model/ReadyToRunDTOs";
+
+@Injectable({
+  providedIn: "root"
+})
+export class ExerciseStepsService {
+  private readonly exerciseStepsBaseUrl = 'webui/exercisesteps';
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  getExerciseSteps(exerciseId: number): Observable<ReadyToRunDTOs.IExerciseSteps> {
+    const url = `${this.exerciseStepsBaseUrl}/${exerciseId}`;
+
+    return this.http.get<ReadyToRunDTOs.IExerciseSteps>(url)
+      .pipe(
+        tap(data => console.log('getExerciseSteps: ' + JSON.stringify(data))),
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage: string;
+
+    if (err.error instanceof Error) {
+        // A client-side or network error occurred. Handle it accordingly.
+        errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong,
+        errorMessage = `Backend returned code ${err.status}, body was: ${err.error}`;
+    }
+    console.error(errorMessage);
+
+    return EMPTY;
+  }
+}
